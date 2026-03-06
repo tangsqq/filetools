46fd4e4587f33e13fb2cf7f57cdbff9b3d5ff227
tangsqq <149044390+tangsqq@users.noreply.github.com>
2026-03-06 13:52:57 +0800

    Update convert.php

--- convert.php
+++ convert.php
@@ -196,7 +196,7 @@ if (isset($_POST["submit"])) {
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📑</text></svg>">
     <title>File Converter</title>
@@ -383,6 +383,7 @@ if (isset($_POST["submit"])) {
 </body>
 
 </html>
+
 
 
 
